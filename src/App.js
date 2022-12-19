4dcf638d70dcd711a1477b379a6944288550dc5d
Dido <krumovds@gmail.com>
2022-12-19 17:11:30 +0200

    Add Changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const expenses = [
     {
-      title: 'Car Insurance',
+      title: "Car Insurance",
       price: 100,
       date: new Date(2022, 12, 2),
-    }
+    },
+    {
+      title: "Car Insurance",
+      price: 100,
+      date: new Date(2022, 12, 2),
+    },
   ];
 
   return (
@@ -17,7 +22,12 @@ function App() {
         price={expenses[0].price}
         date={expenses[0].date}
       ></ExpenseItem>
-         </div>
+      <ExpenseItem
+        title={expenses[0].title}
+        price={expenses[0].price}
+        date={expenses[0].date}
+      ></ExpenseItem>
+    </div>
   );
 }
 
